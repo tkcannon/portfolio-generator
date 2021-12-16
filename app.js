e38c5813b6c87c749524b4c10adf8a4b7b428ed0
tkcannon <taylorkcannon@gmail.com>
2021-12-16 13:39:20 -0700

    Added HTML generation

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('=========================');
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if(err) throw new Error(err);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
