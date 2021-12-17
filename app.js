9a44a401c2232188b6155379592e8000afa1aba5
tkcannon <taylorkcannon@gmail.com>
2021-12-17 10:45:26 -0700

    working in wrong branch

--- app.js
+++ app.js
@@ -141,9 +141,14 @@ promptUser()
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
 
-        fs.writeFile('index.html', pageHTML, err => {
+        fs.writeFile('./dist/index.html', pageHTML, err => {
             if (err) throw new Error(err);
 
             console.log('Portfolio complete! Check out index.html to see the output!');
+
+            fs.copyFile('./src/style.css', './dist/style.css', err => {
+                if (err) throw new Error(err);
+                console.log('Style sheet copied successfully!');
+            })
         });
     });
