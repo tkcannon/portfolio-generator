cd53273fc2d97193fe4aa50e0645bdc7afc63f7b
tkcannon <taylorkcannon@gmail.com>
2021-12-17 11:27:08 -0700

    added promises

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 // const { arrayBuffer } = require('stream/consumers');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
@@ -139,16 +139,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./dist/index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-
-            console.log('Portfolio complete! Check out index.html to see the output!');
-
-            fs.copyFile('./src/style.css', './dist/style.css', err => {
-                if (err) throw new Error(err);
-                console.log('Style sheet copied successfully!');
-            })
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        consolt.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
